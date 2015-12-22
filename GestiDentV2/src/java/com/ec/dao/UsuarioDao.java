c2a5b8b3101a036efbc8da8357858626ac8fb82d
Angel Molina <molinao.angel@gmail.com>
2015-12-22 05:20:19 -0500

    d

---
+++ GestiDentV2/src/java/com/ec/dao/UsuarioDao.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ec.dao;
+
+/**
+ *
+ * @author angel
+ */
+public interface UsuarioDao {
+    public Usuario finByUsuario(Usuario usuario);
+           public Usuario login(); 
+    
+}
